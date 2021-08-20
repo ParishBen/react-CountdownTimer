bb8e96d0b7acac1650cbdfa50e9a912548f7f0fa
Ben <parishben13@gmail.com>
2021-08-20 13:37:06 -0400

    added different variation timer with useState useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Timer from './countdown';
-
+import CountDownTimer from './variationToTimer';
 function App() {
+  const hoursMinSecs = {hours:0, minutes: 10, seconds: 40}
+
   return (
     <div className="App" style={{width:'100%', height:'100%'}} >
       
       <Timer />
+      <CountDownTimer anytime={hoursMinSecs} />
     </div>
   );
 }
